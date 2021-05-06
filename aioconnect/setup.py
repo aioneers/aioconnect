031e188bacb90ad1d86d289a0ab8150ce54ff2fd
aioneersSZ <sebastian.szilvas@aioneers.com>
2021-05-06 11:51:17 +0200

    Also need to change the version

--- aioconnect/setup.py
+++ aioconnect/setup.py
@@ -17,7 +17,7 @@ long_description = "not yet working"
 
 setuptools.setup(
     name="aioconnect",
-    version="0.0.7",
+    version="0.0.8",
     author="AIO",
     author_email="maintainer@aioneers.com",
     license="MIT",
